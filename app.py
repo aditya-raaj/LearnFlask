82f5ee5e147e61971429a97a88dea7521104a2c9
Aditya Raj <rajaditya0024@gmail.com>
2025-05-20 14:24:19 +0530

    update

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ from flask import Flask
 app=Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def home():
     return "<h1>Welcome to home page!</h1>"
 
+@app.route('/about')
+def about():
+    return "<h1>This is the about page</h1>"
 
 if __name__ =="__main__":
     app.run(debug=True)
