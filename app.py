e18457a9610d632f2a17ecc3f8e638c5fbccfa18
Aditya Raj <rajaditya0024@gmail.com>
2025-05-20 14:50:24 +0530

    upload

--- app.py
+++ app.py
@@ -11,5 +11,9 @@ def home():
 def about():
     return "<h1>This is the about page</h1>"
 
+@app.route("/addition/<int:num>")
+def addition(num):
+    return f"Input:{num},Output:{num+10}"
+
 if __name__ =="__main__":
     app.run(debug=True)
